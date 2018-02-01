759b8f6314928ad286045333b09c6c676104010e
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-02-01 01:37:17 -0500

    remove unused code from post page

--- src/pages/post/post.ts
+++ src/pages/post/post.ts
@@ -11,69 +11,8 @@ import marked from 'marked';
 })
 export class PostPage {
 
-  @ViewChild(Slides) slides: Slides;
-
-  data;
-  htmldata;
-
-  step: any;
-  stepCondition: any;
-  stepDefaultCondition: any;
-  currentStep: any;
-  @ViewChild(Content) content: Content;
-  constructor(private viewCtrl: ViewController, public navParams: NavParams, public evts: Events) {
-    /**
-     * Step Wizard Settings
-     */
-    this.step = 1;//The value of the first step, always 1
-    this.stepCondition = false;//Set to true if you don't need condition in every step
-    this.stepDefaultCondition = this.stepCondition;//Save the default condition for every step
-    //You can subscribe to the Event 'step:changed' to handle the current step
-    this.evts.subscribe('step:changed', step => {
-      //Handle the current step if you need
-      this.currentStep = step[0];
-      //Set the step condition to the default value
-      this.stepCondition = this.stepDefaultCondition;
-    });
-    this.evts.subscribe('step:next', () => {
-      //Do something if next
-      console.log('Next pressed: ', this.currentStep);
-    });
-    this.evts.subscribe('step:back', () => {
-      //Do something if back
-      console.log('Back pressed: ', this.currentStep);
-    });
-  }
-
-  /**
-   * Demo functions
-   */
-  onFinish() {
-  }
-
-  toggle() {
-    this.stepCondition = !this.stepCondition;
-  }
-  getIconStep2() {
-    return this.stepCondition ? 'done-all' : 'create';
-  }
-
-  getIconStep3() {
-    return this.stepCondition ? 'happy' : 'sad';
-  }
-  getLikeIcon() {
-    return this.stepCondition ? 'thumbs-down' : 'thumbs-up';
-  }
-  goToExample2() {
-    //this.navCtrl.push(DynamicPage);
-  }
-
-  textChange(e) {
-    if (e.target.value && e.target.value.trim() !== '') {
-      this.stepCondition = true;
-    } else {
-      this.stepCondition = false;
-    }
+  constructor(private viewCtrl: ViewController, public navParams: NavParams) {
+   
   }
 
   convert(this) {
