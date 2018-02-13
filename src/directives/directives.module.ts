4ea9e6b6e78b69a41ab72d09879cef9fac40cd07
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-02-13 02:54:34 -0500

    remove directives again, slow

--- src/directives/directives.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { HidefabDirective } from './hidefab/hidefab';
-@NgModule({
-	declarations: [HidefabDirective],
-	imports: [],
-	exports: [HidefabDirective]
-})
-export class DirectivesModule {}
--- src/pages/TabsSections/feed/feed.module.ts
+++ src/pages/TabsSections/feed/feed.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { FeedPage } from './feed';
 import { ComponentsModule } from '../../../components/components.module';
-import { DirectivesModule } from '../../../directives/directives.module';
 
 @NgModule({
   declarations: [
     FeedPage
   ],
   imports: [
-    DirectivesModule,
     ComponentsModule,
     IonicPageModule.forChild(FeedPage),
   ],
