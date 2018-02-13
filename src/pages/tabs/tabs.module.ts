7f698103d82e1d822862f0f2e369c4429bceba7a
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-02-13 02:14:25 -0500

    remove directives

--- src/pages/tabs/tabs.module.ts
+++ src/pages/tabs/tabs.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TabsPage } from './tabs';
-import { AutoHideDirective } from '../../directives/auto-hide/auto-hide';
 
 
 @NgModule({
   declarations: [
-    TabsPage,
-    AutoHideDirective
+    TabsPage
   ],
   imports: [
     IonicPageModule.forChild(TabsPage),
