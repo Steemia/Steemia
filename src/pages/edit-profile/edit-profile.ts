391f1da441e11202dcb0b3654a1d42ba94b0e6e5
Hüseyin TERKİR <hsynterkr@gmail.com>
2018-03-19 22:12:49 +0300

    edit profile completed

--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -1,12 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the EditProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { BrowserTab } from '@ionic-native/browser-tab';
+import { Component, Input } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController, AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -15,11 +9,66 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EditProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    private browserTab: BrowserTab,
+    public alertCtrl: AlertController,
+    public viewCtrl: ViewController) {
+  }
+
+  private saveInfo(input, value) {
+    this.browserTab.isAvailable()
+    .then((isAvailable: boolean) => {
+
+      if (isAvailable) {
+
+        this.browserTab.openUrl('https://steemconnect.com/sign/profile-update?'+input+'='+value);
+
+      } else {
+
+        // open URL with InAppBrowser instead or SafariViewController
+
+      }
+
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EditProfilePage');
+  }
+
+  public dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+  public showPrompt(input) {
+    let prompt = this.alertCtrl.create({
+      title: 'Edit your'+ input,
+      message: "Click the Save button below to be redirected to SteemConnect to complete your transaction.",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Change Info'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Saved clicked');
+            console.log(data);
+            this.saveInfo(input, data.title);
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }
