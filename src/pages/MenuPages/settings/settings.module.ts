09ebe73b0f1ba162d1329e56b40982ecbe429a54
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-06-16 04:26:31 -0400

    feature(): factory for ngx translate to manipulate global language in settings page

--- src/pages/MenuPages/settings/settings.module.ts
+++ src/pages/MenuPages/settings/settings.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SettingsPage } from './settings';
-import { TranslateModule } from "@ngx-translate/core";
+import { TranslateModule, TranslateLoader} from "@ngx-translate/core";
+import { HttpLoaderFactory } from "../../../app/app.module";
+import { HttpClient } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -9,7 +11,13 @@ import { TranslateModule } from "@ngx-translate/core";
   ],
   imports: [
     IonicPageModule.forChild(SettingsPage),
-    TranslateModule.forChild()
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (HttpLoaderFactory),
+        deps: [HttpClient]
+      }
+    }),
   ],
 })
 export class SettingsPageModule {}
